32bda8632c7bfe672b46a3bfc031d264068b8577
amalbabu1 <amalbabuporuthur@gmail.com>
2022-10-03 23:25:23 +0530

    added error and spinner components

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -10,6 +10,9 @@ import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import { getError } from '../utils';
+import MessageBox from '../components/MessageBox';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -39,16 +42,16 @@ function ProductScreen() {
         const result = await axios.get(`/api/product/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (error) {
-        dispatch({ type: 'FETCH_FAILED', payload: error.message });
+        dispatch({ type: 'FETCH_FAILED', payload: getError(error) });
       }
     };
     fetchdata();
   }, [slug]);
 
   return loading ? (
-    <div>Loading..</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <div>
       <Row>
