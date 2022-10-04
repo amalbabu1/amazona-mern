6472bef58ab8410d3087175fc6e2666900eeae4b
amalbabu1 <amalbabuporuthur@gmail.com>
2022-10-04 23:38:04 +0530

    add to cart

---
+++ frontend/src/Store.js
@@ -0,0 +1,31 @@
+import { createContext, useReducer } from 'react';
+export const Store = createContext();
+
+const initialState = {
+  cart: {
+    cartItems: [],
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CART_ADD_ITEM':
+      // video 2.16.10
+      //   https://www.youtube.com/watch?v=CDtPMR5y0QU
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          cartItems: [...state.cart.cartItems, action.payload],
+        },
+      };
+    default:
+      return state;
+  }
+}
+
+export function StoreProvider(props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
+}
